595401a4abdcf0a9ddb472f7d76fee89b52ff714
Spiider <spiiderdev80@gmail.com>
2025-07-31 20:31:34 -0500

    add level 1 script

--- scripts/levels/level.gd
+++ scripts/levels/level.gd
@@ -5,4 +5,6 @@ class_name Level extends Node2D
 ## Offers basic functionality for all levels and has variables that can specify
 ## how the level is handled by the mains script.
 
-@export var camera_movement_axes: Vector3i = Vector3(0,0,0)
+## Used to specify the axes on which the camera should move to follow the
+## player. (Meant to function as a vector of booleans)
+@export var camera_movement_axes: Vector2i = Vector2i(0,0)
