092db6f7060a101ca050e216476df68d5f3468d1
Spiider <spiiderdev80@gmail.com>
2025-08-02 13:25:28 -0500

    add level-5

--- scripts/levels/level-5.gd
+++ scripts/levels/level-5.gd
@@ -3,8 +3,7 @@ extends Level
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	vertical_gears.append($"Gear-vertical")
-	vertical_gears.append($"Gear-vertical2")
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,9 +13,9 @@ func _process(delta: float) -> void:
 
 func _on_exit_1_body_entered(body: Node2D) -> void:
 	if body is Player:
-		level_progressed.emit(3, 1)
+		level_progressed.emit(4, 1)
 
 
-func _on_exit_2_body_entered(body: Node2D) -> void:
+func _on_frame_portal_body_entered(body: Node2D) -> void:
 	if body is Player:
-		level_progressed.emit(5, 0)
+		level_progressed.emit(6, 0)
