74fbff8b7640144f53080e03a503d82a9102fc1d
Spiider <spiiderdev80@gmail.com>
2025-08-02 00:28:40 -0500

    add exit

--- scripts/levels/level-3.gd
+++ scripts/levels/level-3.gd
@@ -14,3 +14,8 @@ func _process(delta: float) -> void:
 func _on_exit_1_body_entered(body: Node2D) -> void:
 	if body is Player:
 		level_progressed.emit(2, 1)
+
+
+func _on_exit_2_body_entered(body: Node2D) -> void:
+	if body is Player:
+		level_progressed.emit(3, 0)
