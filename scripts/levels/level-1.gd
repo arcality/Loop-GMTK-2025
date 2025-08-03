4ef08f2edf8dc2c52623b6f5e2b040ac85ba67a3
Spiider <spiiderdev80@gmail.com>
2025-08-02 22:36:46 -0500

    connect lvl 6 and 1

--- scripts/levels/level-1.gd
+++ scripts/levels/level-1.gd
@@ -14,3 +14,7 @@ func _process(delta: float) -> void:
 func _on_exit_1_body_entered(body: Node2D) -> void:
 	if body is Player:
 		level_progressed.emit(2, 0)
+
+
+func _on_crosssceneportal_teleport_player() -> void:
+	level_progressed.emit(6, 1)
